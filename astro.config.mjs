f739b17615683e7afeabc2bd041ec1a687398b7c
root <root@vmi2999698.contaboserver.net>
2026-05-22 01:23:06 +0200

    chore: sync server-side portfolio updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 
 export default defineConfig({
-    site: 'https://soyhouston256.github.io',
-    base: 'portfolio',
+    site: 'https://maxflow.ink',
   });
